e6177f16905bd2b7636dc258e0135b2556367574
Shiva Reddy <readytohackk@gmail.com>
2019-04-15 19:18:32 +0530

    updated dependencies and google sign in working

---
+++ app/src/main/java/com/example/zwitter/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.example.zwitter.utils;
+
+public class Constants {
+    public static final String MY_TAG = "myTag";
+    public static final int RC_SIGN_IN = 9001;
+}
---
+++ app/src/main/java/com/example/zwitter/utils/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.zwitter.utils;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.zwitter.R;
+import com.example.zwitter.ui.LoginScreen;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private String mUsername;
+    private String mPhotoUrl;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, LoginScreen.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            if (mFirebaseUser.getPhotoUrl() != null) {
+                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            }
+        }
+    }
+}
